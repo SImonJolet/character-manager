77acd41c1c53996e35b859a66176c2788445d9c4
SImon <jolet.simon@yahoo.com>
2019-09-30 15:52:55 +0200

    Display datas from api in console

--- home.js
+++ home.js
@@ -1,16 +1,19 @@
 "use strict";
 exports.__esModule = true;
-// import axios from 'axios';
 var axios_1 = require("axios");
+var api = "https://character-database.becode.xyz/";
 axios_1["default"]
-  .get("https://character-database.becode.xyz/characters")
-  .then(function(response) {
+    .get(api + "characters")
+    .then(function (response) {
     // handle success
     var result = response.data;
     // console.log(response);
     console.log(result);
-  })
-  ["catch"](function(error) {
+    console.log(result[1].description);
+    for (var i = 0; i < result.length; i++) {
+        console.log("Je suis " + result[i].name + ", je me d\u00E9cris comme \u00E9tant " + result[i].shortDescription);
+    }
+})["catch"](function (error) {
     // handle error
     console.log(error);
-  });
+});
--- home.ts
+++ home.ts
@@ -1,15 +1,20 @@
-// import axios from 'axios';
 import axios from "axios";
-
+const api = "https://character-database.becode.xyz/";
 axios
-  .get("https://character-database.becode.xyz/characters")
-  .then(function(response) {
+  .get(`${api}characters`)
+  .then(function(response: any) {
     // handle success
     let result = response.data;
     // console.log(response);
     console.log(result);
+    console.log(result[1].description);
+    for (let i = 0; i < result.length; i++) {
+      console.log(
+        `Je suis ${result[i].name}, je me décris comme étant ${result[i].shortDescription}`
+      );
+    }
   })
-  .catch(function(error) {
+  .catch(function(error: string) {
     // handle error
     console.log(error);
   });
