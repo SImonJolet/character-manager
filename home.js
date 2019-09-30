277eef7f28bde701d0ed1555875a77471eeb9c6d
SImon <jolet.simon@yahoo.com>
2019-09-30 17:03:01 +0200

    Separate data in screen

--- home.js
+++ home.js
@@ -1,25 +1,42 @@
 "use strict";
 exports.__esModule = true;
 var axios_1 = require("axios");
-var api = "https://character-database.becode.xyz/";
+var api = "https://character-database.becode.xyz";
 axios_1["default"]
-    .get(api + "characters")
+    .get(api + "/characters")
     .then(function (response) {
     // handle success
     var result = response.data;
     // console.log(response);
     console.log(result);
     for (var i = 0; i < result.length; i++) {
+        //boucle dans laquelle, pour chaque item de mon tableau result, je vais aller chercher des données
         var name_1 = result[i].name;
         var shortDescription = result[i].shortDescription;
         var image = result[i].image;
+        //Je definis mes constantes pour chaque item
         console.log(name_1);
         console.log(shortDescription);
         var div = document.createElement("div");
-        var content = document.createTextNode("Bonjour, je suis " + name_1 + " et je me d\u00E9cris comme \u00E9tant " + shortDescription);
-        div.appendChild(content);
+        //Create a container div
+        var containerh1 = document.createElement("h1");
+        //Create a H1 container
+        var contentName = document.createTextNode("Name: " + name_1 + " ");
+        containerh1.appendChild(contentName);
+        //Put the contentName in the h1 container
+        div.appendChild(containerh1);
+        //Connect the h1 container to the div container
+        var containerh2 = document.createElement("h2");
+        //Create a h2 container
+        var contentDescription = document.createTextNode("What am I ? : " + shortDescription);
+        containerh2.appendChild(contentDescription);
+        //Put the contentDescription in the h2 container
+        div.appendChild(containerh2);
+        //Connect the h2 container with the div container
         var currentDiv = document.getElementById("div1");
+        // ???
         document.body.insertBefore(div, currentDiv);
+        // Indicate where insert the new container inthe DOM
     }
 })["catch"](function (error) {
     // handle error
--- home.ts
+++ home.ts
@@ -1,26 +1,50 @@
 import axios from "axios";
-const api = "https://character-database.becode.xyz/";
+const api = "https://character-database.becode.xyz";
 axios
-  .get(`${api}characters`)
+  .get(`${api}/characters`)
   .then(function(response: any) {
     // handle success
     let result = response.data;
     // console.log(response);
     console.log(result);
     for (let i = 0; i < result.length; i++) {
+      //boucle dans laquelle, pour chaque item de mon tableau result, je vais aller chercher des données
       let name = result[i].name;
       let shortDescription = result[i].shortDescription;
       let image = result[i].image;
+      //Je definis mes constantes pour chaque item
       console.log(name);
       console.log(shortDescription);
 
-      let div = document.createElement("div");
-      let content = document.createTextNode(
-        `Bonjour, je suis ${name} et je me décris comme étant ${shortDescription}`
+      let div = document.createElement(`div`);
+      //Create a container div
+
+      let containerh1 = document.createElement("h1");
+      //Create a H1 container
+
+      let contentName = document.createTextNode(`Name: ${name} `);
+      containerh1.appendChild(contentName);
+      //Put the contentName in the h1 container
+
+      div.appendChild(containerh1);
+      //Connect the h1 container to the div container
+
+      let containerh2 = document.createElement("h2");
+      //Create a h2 container
+
+      let contentDescription = document.createTextNode(
+        `What am I ? : ${shortDescription}`
       );
-      div.appendChild(content);
-      var currentDiv = document.getElementById(`div1`);
+      containerh2.appendChild(contentDescription);
+      //Put the contentDescription in the h2 container
+
+      div.appendChild(containerh2);
+      //Connect the h2 container with the div container
+
+      let currentDiv = document.getElementById(`div1`);
+      // ???
       document.body.insertBefore(div, currentDiv);
+      // Indicate where insert the new container inthe DOM
     }
   })
   .catch(function(error: string) {
