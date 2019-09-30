aa6672b712e5a7bb1e6c06d36406a966313c7bd5
SImon <jolet.simon@yahoo.com>
2019-09-30 14:51:04 +0200

    Bof

--- home.js
+++ home.js
@@ -1,23 +1,18 @@
-const axios = require("axios");
-const api = "https://character-database.becode.xyz/";
-
+var axios = require("axios");
+var api = "https://character-database.becode.xyz/";
 // async function getPerso() {
 //     let result = await axios.get(`https://character-database.becode.xyz/characters`);
 //   console.log(result);
-
 //   const array = result.data;
 //   console.log(array);
-
 //   array.forEach((element: { name: string }) => {
 //     console.log(`${element.name}`);
 //   });
 // }
-
 axios
-  .get(`${api}/characters`)
-  .then(function(response) {
+    .get(api + "/characters")
+    .then(function (response) {
     console.log(response);
-  })
-  .catch(function(error) {
+})["catch"](function (error) {
     console.log(error);
-  });
+});
