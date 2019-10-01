a4324ec8882723fd65119c3b1b4fe7099674acec
SImon <jolet.simon@yahoo.com>
2019-10-01 16:59:07 +0200

    End of day

--- home.ts
+++ home.ts
@@ -1,20 +1,38 @@
 import axios from "axios";
 const api = "https://character-database.becode.xyz";
+
 axios
   .get(`${api}/characters`)
   .then(function(response: any) {
     // handle success
     let result = response.data;
+    console.log(result);
 
     const tpl = document.querySelector("#tpl-hero");
     const target = document.querySelector("#target");
 
     const displayHero = el => {
       const elt = tpl.cloneNode(true).content;
+      console.log(el.id);
+
+      async function deleteHero(id: string) {
+        console.log("Do you want to delete it ?");
+        await axios.delete(el);
+      }
+      console.log(deleteHero);
+
+      const edit = () => {};
 
       elt.querySelector(".name").innerHTML = el.name;
       elt.querySelector(".shortDescription").innerHTML = el.shortDescription;
-
+      elt.querySelector("img").src = `data:image/png;base64,${el.image}`;
+      elt.querySelector("#open").onClick = function() {
+        openHero(el.id);
+        console.log(el.id);
+      };
+      elt.querySelector("#delete").onClick = function() {
+        deleteHero(el.id);
+      };
       // elt.querySelector(".powers").innerHTML = el.abilities.join(", ");
 
       target.appendChild(elt);
