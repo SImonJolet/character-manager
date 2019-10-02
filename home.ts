9d8c288599970c438b4634cd36a4108be6fb806d
SImon <jolet.simon@yahoo.com>
2019-10-02 09:39:23 +0200

    -

--- home.ts
+++ home.ts
@@ -15,24 +15,24 @@ axios
       const elt = tpl.cloneNode(true).content;
       console.log(el.id);
 
-      async function deleteHero(id: string) {
-        console.log("Do you want to delete it ?");
-        await axios.delete(el);
-      }
-      console.log(deleteHero);
+      //   async function deleteHero(id: string) {
+      //     console.log("Do you want to delete it ?");
+      //     await axios.delete(el);
+      //   }
+      //   console.log(deleteHero);
 
-      const edit = () => {};
+      //   const edit = () => {};
 
       elt.querySelector(".name").innerHTML = el.name;
       elt.querySelector(".shortDescription").innerHTML = el.shortDescription;
       elt.querySelector("img").src = `data:image/png;base64,${el.image}`;
-      elt.querySelector("#open").onClick = function() {
-        openHero(el.id);
-        console.log(el.id);
-      };
-      elt.querySelector("#delete").onClick = function() {
-        deleteHero(el.id);
-      };
+      // elt.querySelector("#open").onClick = function() {
+      //   openHero(el.id);
+      //   console.log(el.id);
+      // };
+      // elt.querySelector("#delete").onClick = function() {
+      //   deleteHero(el.id);
+      // };
       // elt.querySelector(".powers").innerHTML = el.abilities.join(", ");
 
       target.appendChild(elt);
